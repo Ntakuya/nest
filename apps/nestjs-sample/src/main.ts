4bfc5ec269220aabf2ca0ac74bf813d2a2a2f6f0
長濱拓哉 <nagahamatakuya@nagahamatakuyanoMacBook-Pro.local>
2020-11-09 11:54:40 +0900

    feat(validation) add global validation pipes

--- apps/nestjs-sample/src/main.ts
+++ apps/nestjs-sample/src/main.ts
@@ -2,6 +2,7 @@ import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { HttpExceptionFilter } from './app/core/filters/http-exception.filter';
+import { ValidationPipe } from './app/core/pipes/validation.pipe';
 
 
 async function bootstrap() {
@@ -10,7 +11,11 @@ async function bootstrap() {
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3333;
 
+  // status exception filter
   app.useGlobalFilters(new HttpExceptionFilter())
+
+  // set to global with validaton pipe
+  app.useGlobalPipes(new ValidationPipe())
 
   await app.listen(port, () => {
     Logger.log('Listening at http://localhost:' + port + '/' + globalPrefix);
